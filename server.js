aa86a27268d183febeccb4da356a186711f4ac7b
Kartike <62751693+Scythektk@users.noreply.github.com>
2020-11-04 21:41:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
     res.json({
-        ipaddress: req.ip,
+        ipaddress: (req.header("x-forwarded-for")).split(',')[0],
         language: req.headers['accept-language'],
         software: req.headers['user-agent'],
     });
